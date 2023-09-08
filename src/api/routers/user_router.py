3d250024dbf8145babdc863ef8726c1706ca43b6
Nick Dolomakin <ndolomakin@MacBook-Air-Nick.local>
2023-09-08 16:22:43 +0300

    updated endpoints // will change output

--- src/api/routers/user_router.py
+++ src/api/routers/user_router.py
@@ -33,26 +33,35 @@ async def update_current_user(
     body: UpdateUser,
     Authorize: AuthJWT = Depends(),
     session: AsyncSession = Depends(get_db),
-) -> ShowUser:
-    Authorize.jwt_required()
-    updated_user_params = body.model_dump(exclude_none=True)
-
+):
+    try:
+        Authorize.jwt_required()
+    except:
+        raise HTTPException(
+            status_code=498, detail="Invalid Token"
+        )
+    updated_user_params = body.dict(exclude_none=True)
+    cur_user_username = Authorize.get_jwt_subject()
+    if cur_user_username is None:
+        raise HTTPException(
+            status_code=404, detail=f"User not found."
+        )
     if updated_user_params == {}:
         raise HTTPException(
             status_code=422,
             detail=f"At least one parameter for user update info should be provided",
         )
     try:
-        current_user = Authorize.get_jwt_subject()
+        user = await _get_user_by_username(cur_user_username, session)
         updated_user_id = await _update_user(
             updated_user_params=updated_user_params,
             session=session,
-            user_id=current_user.user_id,
+            user_id=user.user_id,
         )
     except IntegrityError as err:
         logger.error(err)
         raise HTTPException(status_code=503, detail=f"Database error: {err}")
-    return current_user
+    return user
 
 
 @user_router.get("/me")
@@ -96,13 +105,20 @@ async def delete_current_user(
     return DeleteUserResponse(deleted_user_id=deleted_user_id)
 
 
-@user_router.patch("/{user_id}", response_model=ShowUser)
+@user_router.patch("/{user_id}")
 async def update_user_by_id(
     user_id: UUID,
     body: UpdateUser,
+    Authorize: AuthJWT = Depends(),
     session: AsyncSession = Depends(get_db),
 ):
-    updated_user_params = body.model_dump(exclude_none=True)
+    try:
+        Authorize.jwt_required()
+    except:
+        raise HTTPException(
+            status_code=498, detail="Invalid Token"
+        )
+    updated_user_params = body.dict(exclude_none=True)
     if updated_user_params == {}:
         raise HTTPException(
             status_code=422,
@@ -126,15 +142,20 @@ async def update_user_by_id(
 
 
 @user_router.get("/{user_id}")
-async def get_current_user(
+async def get_user_by_id(
     user_id: UUID,
+    Authorize: AuthJWT = Depends(),
     session: AsyncSession = Depends(get_db),
-    cur_user: User = Depends(get_current_user_from_token),
-) -> ShowUser:
-
+):
+    try:
+        Authorize.jwt_required()
+    except:
+        raise HTTPException(
+            status_code=498, detail="Invalid Token"
+        )
     user = await _get_user_by_id(user_id, session)
     if user is None:
         raise HTTPException(
-            status_code=404, detail=f"User with id {cur_user.user_id} not found."
+            status_code=404, detail=f"User with id {user_id} not found."
         )
     return user
